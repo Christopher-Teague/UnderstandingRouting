67049004b27713e4b6620e7bdfff3e3b4e9921f1
Christopher-Teague <cmteague77@gmail.com>
2021-10-27 22:31:12 -0700

    origin

---
+++ server2.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/Dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<name>')
+def hello(name):
+    print(name)
+    return "Hi " + name + "!"
+
+@app.route('/repeat/<number>/<input>')
+def repeat(number, input):
+    x = int(number)
+    print(input) * x
+
+if __name__=="__main__":
+    app.run(debug=True)
+
